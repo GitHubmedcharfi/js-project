39b9fa5d54d685c1c2564a1b037c79a7410606e2
mohamed charfi <charfimedbenfathi@gmail.com>
2024-01-19 11:30:52 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+(function (){
+'use strict'
+document.getElementById('convert').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const distance = document.getElementById("distance").value;
+    const answer = document.getElementById("answer");
+    if (distance) {
+        const conversion = (distance * 1.609344).toFixed(3);
+        answer.innerHTML = `<h2>${distance} miles converts to ${conversion} kilometers</h2>`;
+    } else {
+        answer.innerHTML = '<h2>Please provide a number</h2>';
+    }
+})
+})()
